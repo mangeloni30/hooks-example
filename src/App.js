14f521815080377814d56bb23232da5351f96264
Martin Angeloni <mangeloni@matriz.com.ar>
2020-01-14 18:18:41 -0300

    this is my first commit for hooks

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [name, setMyName] = useState('No name');
+  const [colorClass, setColorClass] = useState('button__div--normal');
+
+  /**
+   * @name clickHandler
+   * method responsible of change the name
+   * with react hooks given a parameter 
+   * @param {string} name
+   * @memberof App
+   * @author Martin Angeloni
+   */
+  const clickHandler = (name) => {
+    setMyName(name);
+  };
+
+    /**
+     *  @name useEffect
+     *  method responsible of set the name state
+     *  call only once
+     *  React Hooks method
+     * @author Martin Angeloni
+     */      
+    useEffect(() => {
+      setMyName('useEffectWithoutName');
+    }, []);
+
+  return (
+    <div className="App">
+      <div className="button-text__div--container">
+        <button 
+          className={colorClass}
+          type="button"
+          onClick={()=>{clickHandler('Martin')}}
+        >
+          Clickme to see the power of react hooks
+        </button>  
+        <p className="text__p--color">
+          {`Click here to change my name!: ${name}`}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
